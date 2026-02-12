2cc39aab9369e294ba307ca1b15561ac5d2b5414
hzezo3499-bit <hzezo3499@gmail.com>
2026-02-12 18:00:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img');
+let continer = document.querySelector('.continer')
+function phone(phone){
+    img.src = phone;
+}
+function colors(color){
+continer.style.background = color;
+}
